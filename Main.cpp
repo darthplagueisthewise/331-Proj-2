4f7f44190d1aa81a2a2d7d286d0a23cdfbb4348f
LukeDKrause <33434650+LukeDKrause@users.noreply.github.com>
2017-12-08 09:45:40 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,21 @@
-/* Main.cpp		*/
+/**
+ * @file Main.cpp
+ * @author Josh Everly
+ * @author Luke Krause
+ * @author Sterling Peschl
+ * @author Jeremy Nelson-Stalmer
+ * @author Tyler Purnick
+ *
+ * This file contains the methods to test the functions of the B+ tree
+ */
 
 #include "Btree.h"
 
+/*
+ * @brief This function tests the functions of the B+ tree
+ *
+ * @return returns 0 on completion
+ */
 int main(int argc, char *argv[])
 {
 	int rec_length=0;
@@ -24,24 +38,25 @@ int main(int argc, char *argv[])
 	  {
 	     if(iRecordPiece != delimiter)
 	     {
-	       iRecord += iRecordPiece;
-				 rec_length++;
+	       	iRecord += iRecordPiece;
+		rec_length++;
 	     }
-	     else						// Do everything else once record is built
-	     	{
-						 numRecs++;
-	           Record newRecord(rec_length, iRecord);
-						 if (numRecs < order+1)
-						 {
-							 newTree.fill_root(newRecord);
-						 }
-						 else
-						 {
-						 newTree.insert(newRecord);
-					   }
-					rec_length = 0;
-	     	}	// end else
-	   }
-	  }
-	   IFile.close();
+	     else			// Do everything else once record is built
+	     {
+		   numRecs++;		// Number of records increases by 1
+	           Record newRecord(rec_length, iRecord);	// Make a new object of the Record class
+		   if (numRecs < order+1)			// Do this until the root splits
+		   {
+			 newTree.fill_root(newRecord);		// ^^
+		   }
+		   else						// Else, do this once root has split
+		   {
+		   newTree.insert(newRecord);			// Insert items until all records have been read in
+	   	   }
+	     rec_length = 0;
+	     }	// end else
+	   }	// end while
+	}	// end if
+	IFile.close();
+	return 0;
 }
