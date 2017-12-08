79f53aa662520a2c095e773f7f6bd559cebdde20
LukeDKrause <33434650+LukeDKrause@users.noreply.github.com>
2017-12-08 09:56:02 -0600

    Update Record.cpp

--- Record.cpp
+++ Record.cpp
@@ -6,6 +6,7 @@
  * @author Jeremy Nelson-Stalmer
  * @author Tyler Purnick
  *
+ * This file contains the methods to store data representing a record
  */
 
 /* Record.cpp  */
@@ -13,13 +14,24 @@
 #include "Record.h"
 using namespace std;
 
+/**
+ * @brief Default constructor
+ *
+ * @return void
+ */
 Record::Record()
 {
 	num = 0;
 	str = "";
 	nptr = nullptr;
 }
-		
+
+/**
+ * @brief Second constructor to be used if children are not known at this time
+ * @param p_int is the integer value belonging to the record
+ * @param p_str is the string value belonging to the record
+ * @return void
+ */
 Record::Record(int p_int, string p_str)
 {
 	num = p_int;
@@ -27,6 +39,13 @@ Record::Record(int p_int, string p_str)
 	nptr = nullptr;
 }
 
+/**
+ * @brief Second constructor to be used if children are not known at this time
+ * @param p_int is the integer value belonging to the record
+ * @param p_str is the string value belonging to the record
+ * @param p_nptr is a node pointer pointing to the record's child node
+ * @return void
+ */
 Record::Record(int p_int, string p_str, Node* p_nptr);
 {
 	num = p_int;
@@ -34,16 +53,31 @@ Record::Record(int p_int, string p_str, Node* p_nptr);
 	nptr = p_nptr;
 }
 
+/**
+ * @brief Deconstructor
+ *
+ * @return void
+ */
 Record::~Record();
 {
 	delete nptr;
 }
 
+/**
+ * @brief Getter function to retrieve the record's integer value
+ *
+ * @return The record's integer value
+ */
 int Node::get_num() const
 {
 	return num;
 }
 
+/**
+ * @brief Getter function to retrieve the record's string value
+ *
+ * @return The record's string value
+ */
 string Node::get_str() const
 {
 	return str;
