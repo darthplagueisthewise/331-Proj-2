19cb9c591e298038cf0a95b2c85db7cbe4dad6cb
LukeDKrause <33434650+LukeDKrause@users.noreply.github.com>
2017-12-07 22:07:40 -0600

    Update Btree.h

--- Btree.h
+++ Btree.h
@@ -1,21 +1,32 @@
-/* Btree.h  */
-/* BTree class */
+/**
+ * @file Btree.h
+ * @author Josh Everly
+ * @author Luke Krause
+ * @author Sterling Peschl
+ * @author Jeremy Nelson-Stalmer
+ *
+ * This file contains the specifications for the methods that build and modify a B+ tree
+ *
+ */
 
 #ifndef _BTREE_H_
 #define _BTREE_H_
 
 #include "Node.h"
 
+/**
+ * @brief This class contains the methods to build and modify a B+ tree
+ */
 class BTree
 {
 	private:
 
-		Node *root;
-		int order;
+		Node *root;		///< The root node
+		int order;		///< The int order value
 
 	public:
 
-		Btree();
+		Btree();		
 		Btree(Node*, int);
 		~Btree();
 
